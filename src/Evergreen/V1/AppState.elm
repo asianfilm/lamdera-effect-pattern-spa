50d43a014588b5bf05b0eb733ff315d01a529c38
Stephen Cremin <stephen@asianfilm.info>
2020-05-26 16:29:18 +0800

    ignore Evergreen until ready to deploy to Lamdera

--- src/Evergreen/V1/AppState.elm
+++
@@ -1,9 +0,0 @@
-module Evergreen.V1.AppState exposing (..)
-
-import Evergreen.V1.Session as Session
-import Url
-
-
-type AppState
-    = NotReady Url.Url
-    | Ready Session.Session
--- src/Evergreen/V1/Env.elm
+++
@@ -1,9 +0,0 @@
-module Evergreen.V1.Env exposing (..)
-
-import Browser.Navigation
-import Time
-
-
-type Env
-    = DevOrProd Int Time.Zone Browser.Navigation.Key
-    | Testing Int Time.Zone
